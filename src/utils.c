f6bc291ecafe62035f6ecc1f5d916abfcfee8553
Kristian Lyngstol <kristian@bohemians.org>
2011-03-11 20:59:26 +0100

    Style-fixes, code-cleanup, check_nrpe port-support
    Removing unused code.

--- src/utils.c
+++ src/utils.c
@@ -1,17 +1,8 @@
-/****************************************************************************
+/*
+ * utils.c - NRPE Utility Functions
  *
- * UTILS.C - NRPE Utility Functions
- *
- * License: GPL
  * Copyright (c) 1999-2006 Ethan Galstad (nagios@nagios.org)
- *
- * Last Modified: 12-11-2006
- *
- * Description:
- *
- * This file contains common network functions used in nrpe and check_nrpe.
- *
- * License Information:
+ * Copyright (c) 2011 Kristian Lyngstol <kristian@bohemians.org>
  *
  * This program is free software; you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -27,7 +18,14 @@
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 675 Mass Ave, Cambridge, MA 02139, USA.
  *
- ****************************************************************************/
+ */
+
+ /*
+  * This file contains common network functions used in nrpe and check_nrpe.
+  * 
+  * XXX: Much legacy or redundant. Fixing it as I go. -K
+  *
+  */
 
 #include "../include/common.h"
 #include "../include/utils.h"
@@ -109,9 +107,11 @@ void randomize_buffer(char *buffer, int buffer_size)
 	return;
 }
 
-#define BUF_SIZE 500
-
-/* opens a connection to a remote host/tcp port */
+/*
+ * Open a connection to the hostname and port and store the FD on *sd.
+ * Returns 0 on success. Iterates sockets until one works, thus
+ * IPv4/IPv6-agnostic.
+ */
 int my_tcp_connect(char *host_name, int port, int *sd)
 {
 	struct addrinfo hints;
@@ -119,228 +119,47 @@ int my_tcp_connect(char *host_name, int port, int *sd)
 	int sfd, s, j;
 	size_t len;
 	ssize_t nread;
-	char buf[BUF_SIZE];
+	char strport[10];
 
-	/* Obtain address(es) matching host/port */
+	s = snprintf(strport,10,"%d",port);
+	if (s>10 || s<0) {
+		fprintf(stderr, "Invalid port-number?");
+		exit(EXIT_FAILURE);
+	}
+	
 	memset(&hints, 0, sizeof(struct addrinfo));
 	hints.ai_family = AF_UNSPEC;
 	hints.ai_socktype = SOCK_STREAM;
 	hints.ai_flags = 0;
 	hints.ai_protocol = 0;
 
-	/*
-	 * FIXME: Port... -K
-	 */
-	s = getaddrinfo(host_name, "5666", &hints, &result);
+	s = getaddrinfo(host_name, strport, &hints, &result);
 	if (s != 0) {
 		fprintf(stderr, "getaddrinfo: %s\n", gai_strerror(s));
 		exit(EXIT_FAILURE);
 	}
 
 	for (rp = result; rp != NULL; rp = rp->ai_next) {
-		sfd = socket(rp->ai_family, rp->ai_socktype,
-				rp->ai_protocol);
+		sfd = socket(rp->ai_family, rp->ai_socktype, rp->ai_protocol);
 		if (sfd == -1)
 			continue;
 
 		if (connect(sfd, rp->ai_addr, rp->ai_addrlen) != -1)
-			break;                  /* Success */
+			break;
 
 		close(sfd);
 	}
 
-	if (rp == NULL) {               /* No address succeeded */
+	if (rp == NULL) {
 		fprintf(stderr, "Could not connect\n");
 		exit(EXIT_FAILURE);
 	}
 
-	freeaddrinfo(result);           /* No longer needed */
+	freeaddrinfo(result);
 
 	*sd = sfd;
 
 	return 0;
-}
-
-/* opens a tcp or udp connection to a remote host */
-int my_connect(char *host_name, int port, int *sd, char *proto)
-{
-	struct sockaddr_in servaddr;
-	struct hostent *hp;
-	struct protoent *ptrp;
-	int result;
-
-	bzero((char *)&servaddr, sizeof(servaddr));
-	servaddr.sin_family = AF_INET;
-	servaddr.sin_port = htons(port);
-
-	/* try to bypass using a DNS lookup if this is just an IP address */
-	if (!my_inet_aton(host_name, &servaddr.sin_addr)) {
-
-		/* else do a DNS lookup */
-		hp = gethostbyname((const char *)host_name);
-		if (hp == NULL) {
-			printf("Invalid host name '%s'\n", host_name);
-			return STATE_UNKNOWN;
-		}
-
-		memcpy(&servaddr.sin_addr, hp->h_addr, hp->h_length);
-	}
-
-	/* map transport protocol name to protocol number */
-	if (((ptrp = getprotobyname(proto))) == NULL) {
-		printf("Cannot map \"%s\" to protocol number\n", proto);
-		return STATE_UNKNOWN;
-	}
-
-	/* create a socket */
-	*sd =
-	    socket(PF_INET, (!strcmp(proto, "udp")) ? SOCK_DGRAM : SOCK_STREAM,
-		   ptrp->p_proto);
-	if (*sd < 0) {
-		printf("Socket creation failed\n");
-		return STATE_UNKNOWN;
-	}
-
-	/* open a connection */
-	result = connect(*sd, (struct sockaddr *)&servaddr, sizeof(servaddr));
-	if (result < 0) {
-		switch (errno) {
-		case ECONNREFUSED:
-			printf("Connection refused by host\n");
-			break;
-		case ETIMEDOUT:
-			printf("Timeout while attempting connection\n");
-			break;
-		case ENETUNREACH:
-			printf("Network is unreachable\n");
-			break;
-		default:
-			printf("Connection refused or timed out\n");
-		}
-
-		return STATE_CRITICAL;
-	}
-
-	return STATE_OK;
-}
-
-/* This code was taken from Fyodor's nmap utility, which was originally taken from
-   the GLIBC 2.0.6 libraries because Solaris doesn't contain the inet_aton() funtion. */
-int my_inet_aton(register const char *cp, struct in_addr *addr)
-{
-	register unsigned int val;	/* changed from u_long --david */
-	register int base, n;
-	register char c;
-	u_int parts[4];
-	register u_int *pp = parts;
-
-	c = *cp;
-
-	for (;;) {
-
-		/*
-		 * Collect number up to ``.''.
-		 * Values are specified as for C:
-		 * 0x=hex, 0=octal, isdigit=decimal.
-		 */
-		if (!isdigit((int)c))
-			return (0);
-		val = 0;
-		base = 10;
-
-		if (c == '0') {
-			c = *++cp;
-			if (c == 'x' || c == 'X')
-				base = 16, c = *++cp;
-			else
-				base = 8;
-		}
-
-		for (;;) {
-			if (isascii((int)c) && isdigit((int)c)) {
-				val = (val * base) + (c - '0');
-				c = *++cp;
-			} else if (base == 16 && isascii((int)c)
-				   && isxdigit((int)c)) {
-				val =
-				    (val << 4) | (c + 10 -
-						  (islower((int)c) ? 'a' :
-						   'A'));
-				c = *++cp;
-			} else
-				break;
-		}
-
-		if (c == '.') {
-
-			/*
-			 * Internet format:
-			 *      a.b.c.d
-			 *      a.b.c   (with c treated as 16 bits)
-			 *      a.b     (with b treated as 24 bits)
-			 */
-			if (pp >= parts + 3)
-				return (0);
-			*pp++ = val;
-			c = *++cp;
-		} else
-			break;
-	}
-
-	/* Check for trailing characters */
-	if (c != '\0' && (!isascii((int)c) || !isspace((int)c)))
-		return (0);
-
-	/* Concoct the address according to the number of parts specified */
-	n = pp - parts + 1;
-	switch (n) {
-
-	case 0:
-		return (0);	/* initial nondigit */
-
-	case 1:		/* a -- 32 bits */
-		break;
-
-	case 2:		/* a.b -- 8.24 bits */
-		if (val > 0xffffff)
-			return (0);
-		val |= parts[0] << 24;
-		break;
-
-	case 3:		/* a.b.c -- 8.8.16 bits */
-		if (val > 0xffff)
-			return (0);
-		val |= (parts[0] << 24) | (parts[1] << 16);
-		break;
-
-	case 4:		/* a.b.c.d -- 8.8.8.8 bits */
-		if (val > 0xff)
-			return (0);
-		val |= (parts[0] << 24) | (parts[1] << 16) | (parts[2] << 8);
-		break;
-	}
-
-	if (addr)
-		addr->s_addr = htonl(val);
-
-	return (1);
-}
-
-void strip(char *buffer)
-{
-	int x;
-	int index;
-
-	for (x = strlen(buffer); x >= 1; x--) {
-		index = x - 1;
-		if (buffer[index] == ' ' || buffer[index] == '\r'
-		    || buffer[index] == '\n' || buffer[index] == '\t')
-			buffer[index] = '\x0';
-		else
-			break;
-	}
-
-	return;
 }
 
 /* sends all data - thanks to Beej's Guide to Network Programming */
